f6000bb60469cf158d66314f837e5276e71a122e
Jonathan Hook <jonathan.hook@york.ac.uk>
2023-02-27 12:57:17 +0000

    Added project files

---
+++ Assets/Practical Assets/DevicePose.cs
@@ -0,0 +1,49 @@
+﻿/*
+ * Script based on code from:
+ * https://forum.unity3d.com/threads/sharing-gyroscope-camera-script-ios-tested.241825/
+ * https://gist.github.com/BichengLUO/32091aecf9f189a24953840e32c060cf
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class DevicePose : MonoBehaviour
+{
+	public float accelSensitivity = 0.1f;
+	private Vector3 lpAccel;
+	private Quaternion initialRotation;
+
+	void Start()
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;
+
+		if (SystemInfo.supportsGyroscope)
+		{
+			Input.gyro.enabled = true;
+		}
+		else
+		{
+			lpAccel = Vector3.zero;
+			initialRotation = transform.rotation;
+		}
+	}
+
+	void Update()
+	{
+		if (SystemInfo.supportsGyroscope)
+		{
+			transform.rotation = Input.gyro.attitude;
+			transform.Rotate(0.0f, 0.0f, 180.0f, Space.Self);
+			transform.Rotate(90.0f, 180.0f, 0.0f, Space.World);
+		}
+		else
+		{
+			Vector3 acc = Input.acceleration;
+			lpAccel = ((1.0f - accelSensitivity) * lpAccel) + (accelSensitivity * acc);
+
+			transform.rotation = initialRotation;
+			transform.RotateAround(transform.position, Vector3.forward, -lpAccel.x * 90.0f);
+			transform.RotateAround(transform.position, Vector3.right, -lpAccel.z * 90.0f);
+		}
+	}
+}
---
+++ Assets/Practical Assets/WebcamRender.cs
@@ -0,0 +1,29 @@
+﻿/*
+ * Script based on code from:
+ * http://answers.unity3d.com/questions/909967/getting-a-web-cam-to-play-on-ui-texture-image.html
+ */
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(RawImage))]
+public class WebcamRender : MonoBehaviour 
+{
+	private RawImage img;
+	private WebCamTexture cam;
+
+	void Start () 
+	{
+		Screen.orientation = ScreenOrientation.LandscapeLeft;			
+		img = GetComponent<RawImage> ();
+		InitWebcamTexture ();
+	}
+
+	private void InitWebcamTexture()
+	{
+		cam = new WebCamTexture ();
+		img.texture = cam;
+		img.material.mainTexture = cam;
+		cam.Play();
+	}
+}
